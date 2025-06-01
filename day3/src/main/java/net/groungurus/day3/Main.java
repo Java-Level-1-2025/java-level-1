94b4cc4d0a909b8c22af14d21033b4edb0972de2
Philip Mark Gutierrez <sensei_slayer@protonmail.com>
2025-06-01 12:36:47 +0800

    day 3

---
+++ day3/src/main/java/net/groungurus/day3/Main.java
@@ -0,0 +1,7 @@
+import net.groungurus.day3.Shop;
+
+void main() {
+    var goldilocks = new Shop();
+    goldilocks.name = "Goldilocks";
+    System.out.println(goldilocks);
+}
---
+++ day3/src/main/java/net/groungurus/day3/Shop.java
@@ -0,0 +1,11 @@
+package net.groungurus.day3;
+
+public class Shop {
+    public String name;
+    public String[] items;
+
+    @Override
+    public String toString() { // method overloading
+        return name;
+    }
+}
